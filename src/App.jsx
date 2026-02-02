43f710a154a0ff6c7a1bfcdca80c640c8620dd15
monarchxr <raunakjais2131@gmail.com>
2026-02-02 16:15:55 +0530

    basic component structure set

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function HeadBox(){
+  return(
+    <>
+      <img></img>
+      <h2>title</h2>
+    </>
+  )
+}
 
+function WeatherBox(){
+  return(
+    <>
+      <h2>weatherbox</h2>
+      <div>
+        <SearchBox />
+        <CurrentWeatherBox />
+        <ForecastBox />
+      </div>
+
+
+    </>
+  )
+}
+
+function SearchBox(){
+  return(
+    <div>searchbox</div>
+  )
+}
+
+function CurrentWeatherBox(){
+  return(
+    <div>current weather box</div>
+  )
+}
+
+function ForecastBox(){
+  return(
+    <div>forecast box</div>
+  )
+}
+
+function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <HeadBox />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <WeatherBox />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
