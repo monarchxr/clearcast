349d047f4c4018c56e64a8d0911195dfeea530bd
monarchxr <raunakjais2131@gmail.com>
2026-04-16 15:53:02 +0530

    Initialized frontend with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App(){
+    return(
+        <>
+            <Header />
+            <Dashboard>
+                <CurrentWeather />
+                <Forecast />
+                <Astronomy />
+            </Dashboard>
+            <Footer />
+            
+        </>
+    )
+}
